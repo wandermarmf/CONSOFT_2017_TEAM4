4c7ed9314aa8c34abf04eb9c45190dc6395c96a3
marco-cardoso <marcom.c@outlook.com>
2017-07-10 16:18:36 -0400

    atributos da classe conferencia

--- SGCteam4/src/main/java/br/gov/ufms/SGCteam04/models/Conferencia.java
+++ SGCteam4/src/main/java/br/gov/ufms/SGCteam04/models/Conferencia.java
@@ -1,17 +1,102 @@
 package br.gov.ufms.SGCteam04.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+
 @Entity
 public class Conferencia {
 
 	   @Id
-	   @GeneratedValue(strategy = GenerationType.IDENTITY)
+	   @GeneratedValue(strategy = GenerationType.AUTO)
 	   private Integer id;
+
+	   @Column(unique=true,nullable = false)
 	   private String nomeAbreviado;
-	   
+
+	   @Column(unique = true,nullable = false)
+	   private String nomeConferencia;
+
+	   @Column(nullable = false)
+	   private String subTitulo;
+
+	   private String subTituloSecundario;
+
+	   private String paginaWeb;
+
+	   @Column(nullable = false)
+	   private String cidadeConferencia;
+
+	   @Column(nullable = false)
+	   private String cidadeEmissaoFatura;
+
+	   @Column(nullable = false)
+	   private Double valorTaxaEmissaoFatura;
+
+	   @Column(nullable = false)
+	   private String moedaConferencia;
+
+	   @ManyToMany
+	   private ArrayList<Topico> topicoArrayList;
+
+	   @ManyToMany
+	   private ArrayList<OpcaoPagamento> opcaoPagamentoArrayList;
+
+	   /*
+	   TODO criar entidade e repositorio da sessao
+	   @OneToMany
+	   private ArrayList<Sessao> sessaoArrayList;
+		*/
+
+	   /*
+	   TODO criar entidade e repositorio da fase da conferencia
+	   @OneToMany
+	   private ArrayList<ConferenciaFase> conferenciaFaseArrayList;
+		*/
+
+	   /*
+	   TODO criar entidade e repositorio do evento/produto da conferencia
+	   @OneToMany
+	   private ArrayList<ConferenciaEventoProduto> conferenciaEventoProdutoArrayList;
+		*/
+
+	   /*
+	   	TODO criar entidade e repositorio do intervalo desconto da conferencia
+	   	@OneTo?????
+	   	private ArrayList<IntervaloDescontoConferencia> ...
+	    */
+
+	   /*
+	   	TODO criar entidade e repositorio da inscricao
+	   	@OneToMany
+	   	private ArrayList<Inscricao> inscricaoArrayList;
+	    */
+
+	   /*
+	   TODO criar entidade e repositorio do coordenador
+	   @OneTo????
+	   private ....
+	    */
+
+	   /*
+	   TODO criar entidade e repositorio do administrador
+	   @OneTo????
+	   private ....
+	    */
+
+	   @OneToMany
+	   private ArrayList<TipoSubmissao> tipoSubmissaoArrayList;
+
+	   @OneToMany
+	   private ArrayList<TipoArquivo> tipoArquivoArrayList;
+
+	   /*
+	   TODO criar entidade e repositorio do perfil
+	   @ManyToMany
+	   private ArrayList<Perfil> perfilArrayList;
+	    */
+
+
+
 	   public Conferencia() {
 		// TODO Auto-generated constructor stub
 	}
