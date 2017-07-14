86d8363164a2f1e3194a5160289fa0fb821bf0f7
marco-cardoso <marcom.c@outlook.com>
2017-07-13 20:44:08 -0400

    atualizacao
    classe conferencia fase

---
+++ SGCteam4/src/main/java/br/gov/ufms/SGCteam04/models/ConferenciaFase.java
@@ -0,0 +1,54 @@
+package br.gov.ufms.SGCteam04.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Marco Cardoso on 7/13/2017.
+ */
+@Entity
+public class ConferenciaFase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Temporal(TemporalType.DATE)
+    private Date periodo_inciial;
+    private Date periodo_final;
+    private Boolean faseDesativada;
+    private String descricao;
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getPeriodo_inciial() {
+        return periodo_inciial;
+    }
+
+    public void setPeriodo_inciial(Date periodo_inciial) {
+        this.periodo_inciial = periodo_inciial;
+    }
+
+    public Date getPeriodo_final() {
+        return periodo_final;
+    }
+
+    public void setPeriodo_final(Date periodo_final) {
+        this.periodo_final = periodo_final;
+    }
+
+    public Boolean getFaseDesativada() {
+        return faseDesativada;
+    }
+
+    public void setFaseDesativada(Boolean faseDesativada) {
+        this.faseDesativada = faseDesativada;
+    }
+}
---
+++ SGCteam4/src/main/java/br/gov/ufms/SGCteam04/repositories/ConferenciaFaseRepository.java
@@ -0,0 +1,10 @@
+package br.gov.ufms.SGCteam04.repositories;
+
+import br.gov.ufms.SGCteam04.models.ConferenciaFase;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Marco Cardoso on 7/13/2017.
+ */
+public interface ConferenciaFaseRepository extends CrudRepository<ConferenciaFase,Long> {
+}
